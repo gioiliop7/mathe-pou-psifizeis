8e44ce381c4d4cd9245987039d0757a889875681
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2023-05-16 01:29:25 +0300

    countdown text

--- app/components/Countdown/Countdown.jsx
+++ app/components/Countdown/Countdown.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Countdown = ({ targetDate }) => {
   const [countdown, setCountdown] = useState(null);
+  const [countdownExists, setExistance] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -11,6 +12,7 @@ const Countdown = ({ targetDate }) => {
       if (distance <= 0) {
         clearInterval(interval);
         setCountdown("Countdown complete!");
+        setExistance(false);
       } else {
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor(
@@ -20,16 +22,26 @@ const Countdown = ({ targetDate }) => {
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         setCountdown(`${days}Η ${hours}Ω ${minutes}Λ ${seconds}Δ`);
+        setExistance(true);
       }
     }, 1000);
 
     return () => clearInterval(interval);
   }, [targetDate]);
 
+  console.log(countdownExists);
+
   return (
-    <div className="bg-blue-300 w-full mx-auto flex justify-center max-w-[300px] py-4 text-white text-3xl font-bold rounded-xl text-center">
-      {countdown}
-    </div>
+    <>
+      {countdownExists && (
+        <p className="text-base text-blue-700 text-center font-bold">
+          Επόμενη εκλογική αναμέτρηση
+        </p>
+      )}
+      <div className="bg-blue-300 w-full mx-auto flex justify-center max-w-[300px] py-4 text-white text-3xl font-bold rounded-xl text-center">
+        {countdown}
+      </div>
+    </>
   );
 };
 
